7c6f66b746e9f36161cf56304cbb1172472b6253
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-04-16 23:02:06 +0700

    crud master kategori

---
+++ MiniMarket/MasterKategoriBarang.Designer.vb
@@ -0,0 +1,166 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MasterKategoriBarang
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btn_tutup = New System.Windows.Forms.Button()
+        Me.btn_hapus = New System.Windows.Forms.Button()
+        Me.btn_edit = New System.Windows.Forms.Button()
+        Me.btn_input = New System.Windows.Forms.Button()
+        Me.txt_keterangan = New System.Windows.Forms.TextBox()
+        Me.txt_nama = New System.Windows.Forms.TextBox()
+        Me.txt_kode = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(260, 38)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(329, 238)
+        Me.DataGridView1.TabIndex = 29
+        '
+        'btn_tutup
+        '
+        Me.btn_tutup.Location = New System.Drawing.Point(125, 253)
+        Me.btn_tutup.Name = "btn_tutup"
+        Me.btn_tutup.Size = New System.Drawing.Size(75, 23)
+        Me.btn_tutup.TabIndex = 7
+        Me.btn_tutup.Text = "Tutup"
+        Me.btn_tutup.UseVisualStyleBackColor = True
+        '
+        'btn_hapus
+        '
+        Me.btn_hapus.Location = New System.Drawing.Point(23, 253)
+        Me.btn_hapus.Name = "btn_hapus"
+        Me.btn_hapus.Size = New System.Drawing.Size(75, 23)
+        Me.btn_hapus.TabIndex = 6
+        Me.btn_hapus.Text = "Hapus"
+        Me.btn_hapus.UseVisualStyleBackColor = True
+        '
+        'btn_edit
+        '
+        Me.btn_edit.Location = New System.Drawing.Point(125, 198)
+        Me.btn_edit.Name = "btn_edit"
+        Me.btn_edit.Size = New System.Drawing.Size(75, 23)
+        Me.btn_edit.TabIndex = 5
+        Me.btn_edit.Text = "Edit"
+        Me.btn_edit.UseVisualStyleBackColor = True
+        '
+        'btn_input
+        '
+        Me.btn_input.Location = New System.Drawing.Point(23, 198)
+        Me.btn_input.Name = "btn_input"
+        Me.btn_input.Size = New System.Drawing.Size(75, 23)
+        Me.btn_input.TabIndex = 4
+        Me.btn_input.Text = "Input"
+        Me.btn_input.UseVisualStyleBackColor = True
+        '
+        'txt_keterangan
+        '
+        Me.txt_keterangan.Location = New System.Drawing.Point(111, 112)
+        Me.txt_keterangan.Name = "txt_keterangan"
+        Me.txt_keterangan.Size = New System.Drawing.Size(100, 20)
+        Me.txt_keterangan.TabIndex = 3
+        '
+        'txt_nama
+        '
+        Me.txt_nama.Location = New System.Drawing.Point(111, 76)
+        Me.txt_nama.Name = "txt_nama"
+        Me.txt_nama.Size = New System.Drawing.Size(100, 20)
+        Me.txt_nama.TabIndex = 2
+        '
+        'txt_kode
+        '
+        Me.txt_kode.Location = New System.Drawing.Point(111, 38)
+        Me.txt_kode.Name = "txt_kode"
+        Me.txt_kode.Size = New System.Drawing.Size(100, 20)
+        Me.txt_kode.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 115)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 13)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Keterangan"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 28
+        Me.Label2.Text = "Nama"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(20, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(32, 13)
+        Me.Label1.TabIndex = 27
+        Me.Label1.Text = "Kode"
+        '
+        'MasterKategoriBarang
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(615, 305)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btn_tutup)
+        Me.Controls.Add(Me.btn_hapus)
+        Me.Controls.Add(Me.btn_edit)
+        Me.Controls.Add(Me.btn_input)
+        Me.Controls.Add(Me.txt_keterangan)
+        Me.Controls.Add(Me.txt_nama)
+        Me.Controls.Add(Me.txt_kode)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MasterKategoriBarang"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MasterKategoriBarang"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents btn_tutup As System.Windows.Forms.Button
+    Friend WithEvents btn_hapus As System.Windows.Forms.Button
+    Friend WithEvents btn_edit As System.Windows.Forms.Button
+    Friend WithEvents btn_input As System.Windows.Forms.Button
+    Friend WithEvents txt_keterangan As System.Windows.Forms.TextBox
+    Friend WithEvents txt_nama As System.Windows.Forms.TextBox
+    Friend WithEvents txt_kode As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+End Class
---
+++ MiniMarket/MasterKategoriBarang.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Data.OleDb
+Public Class MasterKategoriBarang
+
+    Sub kondisiawal()
+        txt_kode.Text = ""
+        txt_nama.Text = ""
+        txt_keterangan.Text = ""
+        btn_input.Text = "Input"
+        btn_edit.Text = "Edit"
+        btn_hapus.Text = "Hapus"
+        btn_tutup.Text = "Tutup"
+        btn_input.Enabled = True
+        btn_edit.Enabled = True
+        btn_hapus.Enabled = True
+        btn_tutup.Enabled = True
+        Call munculgrid()
+    End Sub
+
+    Sub munculgrid()
+        Call koneksi()
+        da = New OleDbDataAdapter("select * from tbl_kategori", conn)
+        ds = New DataSet
+        ds.Clear()
+        da.Fill(ds, "tbl_kategori")
+        DataGridView1.DataSource = (ds.Tables("tbl_kategori"))
+    End Sub
+
+    Private Sub MasterKategoriBarang_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Call kondisiawal()
+    End Sub
+
+    Private Sub btn_input_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_input.Click
+        If btn_input.Text = "Input" Then
+            btn_input.Text = "Simpan"
+            btn_edit.Enabled = False
+            btn_hapus.Enabled = False
+            btn_tutup.Text = "Batal"
+        Else
+            If txt_kode.Text = "" Or txt_nama.Text = "" Or txt_keterangan.Text = "" Then
+                MsgBox("Isi data dengan benar!")
+            Else
+                Call koneksi()
+                Dim simpandata As String = "insert into tbl_kategori values ('" & txt_kode.Text & "','" & txt_nama.Text & "','" & txt_keterangan.Text & "')"
+                cmd = New OleDbCommand(simpandata, conn)
+                cmd.ExecuteNonQuery()
+                MsgBox("Data berhasil ditambahkan")
+                Call kondisiawal()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_tutup_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_tutup.Click
+        If btn_tutup.Text = "Batal" Then
+            Call kondisiawal()
+        Else
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub txt_kode_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txt_kode.KeyPress
+        If e.KeyChar = Chr(13) Then
+            Call koneksi()
+            cmd = New OleDbCommand("select * from tbl_kategori where kodekategori = '" & txt_kode.Text & "'", conn)
+            rd = cmd.ExecuteReader
+            rd.Read()
+            If rd.HasRows Then
+                txt_nama.Text = rd.Item("nm_kategori")
+                txt_keterangan.Text = rd.Item("ket_kategori")
+            Else
+                MsgBox("Data tidak ditemukan!")
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_edit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_edit.Click
+        If btn_edit.Text = "Edit" Then
+            btn_edit.Text = "Simpan"
+            btn_input.Enabled = False
+            btn_hapus.Enabled = False
+            btn_tutup.Text = "Batal"
+        Else
+            If txt_kode.Text = "" Or txt_nama.Text = "" Or txt_keterangan.Text = "" Then
+                MsgBox("Isi data dengan benar!")
+            Else
+                Call koneksi()
+                Dim editdata As String = "update tbl_kategori set nm_kategori='" & txt_nama.Text & "', ket_kategori='" & txt_keterangan.Text & "' where kodekategori='" & txt_kode.Text & "'"
+                cmd = New OleDbCommand(editdata, conn)
+                cmd.ExecuteNonQuery()
+                MsgBox("Data berhasil diubah")
+                Call kondisiawal()
+            End If
+        End If
+    End Sub
+
+    Private Sub btn_hapus_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btn_hapus.Click
+        If btn_hapus.Text = "Hapus" Then
+            btn_hapus.Text = "Hapus Data"
+            btn_edit.Enabled = False
+            btn_input.Enabled = False
+            btn_tutup.Text = "Batal"
+        Else
+            If txt_kode.Text = "" Or txt_nama.Text = "" Or txt_keterangan.Text = "" Then
+                MsgBox("Isi data dengan benar!")
+            Else
+                Call koneksi()
+                Dim hapusdata As String = "delete from tbl_kategori where kodekategori='" & txt_kode.Text & "'"
+                cmd = New OleDbCommand(hapusdata, conn)
+                cmd.ExecuteNonQuery()
+                MsgBox("Data berhasil dihapus")
+                Call kondisiawal()
+            End If
+        End If
+    End Sub
+End Class
